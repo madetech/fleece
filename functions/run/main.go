8d75480f552954676cfb8df6eabd42778bdd2dbc
David Winter <i@djw.me>
2016-08-24 12:29:49 +0100

    Allow command name to be passed in via args

--- functions/run/main.go
+++ functions/run/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/apex/go-apex"
-  "os/exec"
+	"os"
+	"os/exec"
 )
 
 type commandOutput struct {
-  Output string `json:"output"`
+	Output string `json:"output"`
+}
+
+type commandInput struct {
+	Cmd  string `json:"cmd"`
+	Args string `json:"args"`
 }
 
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-    var m commandOutput
+		var cmdIn commandInput
+		json.Unmarshal(event, &cmdIn)
+		fmt.Fprintf(os.Stderr, cmdIn.Cmd)
+		fmt.Fprintf(os.Stderr, cmdIn.Args)
 
-    test := exec.Command("./test")
-    output, _ := test.Output()
-    m.Output = string(output)
+		var m commandOutput
+		test := exec.Command(fmt.Sprintf("./%s", cmdIn.Cmd))
+		output, _ := test.Output()
+		m.Output = string(output)
 
 		return m, nil
 	})
